f41df130fb67f0fb706f0758b14bb674f27c7d4f
sateesh0584 <143077082+sateesh0584@users.noreply.github.com>
2024-07-12 17:52:33 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull sateesh0584/simple-python-flask:latest
 
 # Run the Docker image as a container
-docker run -d -p 6000:6000 sateesh0584/simple-python-flask:latest
+docker run -d -p 5000:5000 sateesh0584/simple-python-flask:latest
