96f8f8c5ea60d674893c1fea53867314bba186e5
sateesh0584 <143077082+sateesh0584@users.noreply.github.com>
2024-07-12 16:45:41 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,6 +1,13 @@
-
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Get the Docker container ID of the running container
+containerID=$(docker ps -q --filter ancestor=sateesh0584/simple-python-flask)
+
+# Stop and remove the container if it exists
+if [ ! -z "$containerID" ]; then
+    docker rm -f "$containerID"
+    echo "Container $containerID stopped and removed."
+else
+    echo "No running container found."
+fi
