e502e55a23ff6fdc4cfafc814cfe969cbdd58b44
sateesh0584 <143077082+sateesh0584@users.noreply.github.com>
2024-07-12 17:22:57 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull sateesh0584/simple-python-flask
+docker pull sateesh0584/shopfront
 
 # Run the Docker image as a container
-docker run -d -p 6000:6000 sateesh0584/simple-python-flask
+docker run -d -p 6000:6000 sateesh0584/shopfront
