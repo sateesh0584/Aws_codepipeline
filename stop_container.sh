4ca7af286c0e2a25273d6bebb2118c6aa2b4b2b1
sateesh0584 <143077082+sateesh0584@users.noreply.github.com>
2024-07-12 18:07:00 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -6,7 +6,7 @@ containerID=$(docker ps -q --filter ancestor=sateesh0584/simple-python-flask)
 
 # Stop and remove the container if it exists
 if [ ! -z "$containerID" ]; then
-    docker rm -f "$containerID"
+    docker rm -f "$containerID" 
     echo "Container $containerID stopped and removed."
 else
     echo "No running container found."
