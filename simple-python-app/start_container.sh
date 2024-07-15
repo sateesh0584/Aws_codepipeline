11101437f5b9123361b8b2abda581b767db89167
sateesh0584 <143077082+sateesh0584@users.noreply.github.com>
2024-07-15 17:21:28 +0530

    Update start_container.sh

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e 
 
 # Pull the Docker image from Docker Hub
-docker pull sateesh0584/simple-python-flask
+docker pull sateesh0584/simple-python-flask 
   
 # Run the Docker image as a container
 docker run -d -p 7000:7000 sateesh0584/simple-python-flask 
